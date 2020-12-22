0413ff4da98477e4f4e3496a339a7ac2bb12a6d3
cherry42 <542547787@qq.com>
2020-12-22 23:17:12 +0800

    testng

---
+++ src/main/java/sum.java
@@ -0,0 +1,7 @@
+import java.nio.IntBuffer;
+
+public class sum {
+    public int add(int a, int b){
+        return  (a+b);
+    }
+}
---
+++ src/test/java/sumTest.java
@@ -0,0 +1,14 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class sumTest {
+    @Test
+    public void test(){
+        int expect=11;
+        sum sum = new sum();
+        int add = sum.add(5, 6);
+        Assert.assertEquals(add,expect);
+
+
+    }
+}
